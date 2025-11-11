ebf567592554e281e7dd70e0e0570c41f8a3bbfa
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-11 20:34:05 -0300

    feat(resources): add ClientResource.java

--- backend/src/main/java/com/aceleradev/backend/resources/ClientResource.java
+++ backend/src/main/java/com/aceleradev/backend/resources/ClientResource.java
@@ -1,4 +1,21 @@
 package com.aceleradev.backend.resources;
 
+import com.aceleradev.backend.entities.Client;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientResource {
+
+    @GetMapping
+    public ResponseEntity<Client> findAll(){
+
+        Client pc = new Client(1L, "Pc The One", "12982228898", "pctheone@email.com", "00000000001", "O melhorzin que tá tendo",new Date());
+        return ResponseEntity.ok().body(pc);
+    }
 }
